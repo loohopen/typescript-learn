d901d0e214df2dd403b82acc15be59841bf1abad
卢虎鹏 <hopen@luhupengdeMacBook-Pro.local>
2019-02-25 17:56:17 +0800

    联合类型学习

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -10,3 +10,16 @@ export class HelloComponents extends React.Component<HelloProps, {}> {
         return <h1>Hello from {this.props.compiler}, and test from {this.props.framework}</h1>
     }
 }
+
+// 联合类型
+export function paddingLeft(value: string, padding: number | string): string{
+    if(typeof padding == 'number'){
+        return Array(padding + 1).join(" ") + value;
+    }
+    if(typeof padding == 'string'){
+        return padding + value;
+    }
+    throw new Error(`excepted string or number, got ${padding}`);
+}
+
+paddingLeft('Hello', 7);
